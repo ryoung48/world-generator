90036323b5f8870c99406fe0969d47cbc48482a3
Ryan Young <rayoung1093@gmail.com>
2022-08-14 19:15:04 -0400

    fix eslint

--- src/models/npcs/species/primordials/traits/finalize.ts
+++ src/models/npcs/species/primordials/traits/finalize.ts
@@ -4,7 +4,7 @@ import { primordial__behavior_trait_finalize } from './behavior/finalize'
 import { PrimordialTraitParams } from './types'
 
 export const primordial__trait_finalize: Partial<
-  Record<primordial__trait_tags, (params: PrimordialTraitParams) => void>
+  Record<primordial__trait_tags, (_params: PrimordialTraitParams) => void>
 > = {
   ...primordial__behavior_trait_finalize,
   ...primordial__aesthetic_trait_finalize
