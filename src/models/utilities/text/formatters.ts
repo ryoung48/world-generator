f0f57a59c8e0a4469a6fdaa20fab7ad5101082dc
Ryan Young <rayoung1093@gmail.com>
2023-03-07 21:32:18 -0500

    fix UTs

--- src/models/utilities/text/formatters.ts
+++ src/models/utilities/text/formatters.ts
@@ -1,5 +1,3 @@
-import * as dayjs from 'dayjs'
-
 const local = 'en-US'
 export const formatters = {
   percent: (value: number, precision = 0) =>
@@ -7,13 +5,5 @@ export const formatters = {
       value
     ),
   compact: (value: number) =>
-    new Intl.NumberFormat(local, { notation: 'compact' } as any).format(value),
-  date: (value: number) => dayjs(value).format('MM/DD/YYYY'),
-  time: (value: number) => dayjs(value).format('h:mm A'),
-  dateTime: (value: number) => dayjs(value).format('MM/DD/YYYY, h:mm A'),
-  dateRange: (params: { start: number; end?: number }) => {
-    const start = formatters.date(params.start)
-    const end = Number.isFinite(params.end) ? formatters.date(params.end) : 'Present'
-    return `${start} - ${end}`
-  }
+    new Intl.NumberFormat(local, { notation: 'compact' } as any).format(value)
 }
--- tests/models/utilities/text/formatters.test.ts
+++ tests/models/utilities/text/formatters.test.ts
@@ -8,24 +8,4 @@ test('formatters: compact', () => {
   const result = formatters.compact(9812981298129812)
   expect(result).toEqual('9813T')
 })
-test('formatters: date', () => {
-  const result = formatters.date(0)
-  expect(result).toEqual('12/31/1969')
-})
-test('formatters: time', () => {
-  const result = formatters.time(0)
-  expect(result).toEqual('7:00 PM')
-})
-test('formatters: date time', () => {
-  const result = formatters.dateTime(0)
-  expect(result).toEqual('12/31/1969, 7:00 PM')
-})
-test('formatters: past date range', () => {
-  const result = formatters.dateRange({ start: 0, end: 10000000 })
-  expect(result).toEqual('12/31/1969 - 12/31/1969')
-})
-test('formatters: future date range', () => {
-  const result = formatters.dateRange({ start: 0, end: Infinity })
-  expect(result).toEqual('12/31/1969 - Present')
-})
 export {}
